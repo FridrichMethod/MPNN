0eb91f53ac94432e8af6a952dd02852241fe48e4
FridrichMethod <zhaoyangli@stanford.edu>
2025-12-10 21:26:46 -0800

    Update training script to enhance model training configuration
    - Replaced the original training command with a more comprehensive setup using the `mpnn.training` module.
    - Added various hyperparameters including optimizer, scheduler, and model architecture details to improve training performance.
    - Updated the path for training data to use the project root directory for better portability.

--- scripts/train_example.sh
+++ scripts/train_example.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-python src/training.py --path_for_training_data ~/data/mpnn/raw/pdb_2021aug02/ --wandb --wandb_project "pretraining-scaling" --wandb_entity "stanford-protein" --run_name "mpnn_original_implementation"
+PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
+
+python -m mpnn.training \
+    --path_for_training_data "$PROJECT_ROOT_DIR"/datasets/pdb_2021aug02_sample/ \
+    --wandb \
+    --wandb_project mpnn-features \
+    --wandb_entity stanford-protein \
+    --optimizer adamw \
+    --scheduler cosine \
+    --hidden_dim 128 \
+    --num_encoder_layers 3 \
+    --num_decoder_layers 3 \
+    --num_neighbors 48 \
+    --max_protein_length 10000 \
+    --backbone_noise 0.2 \
+    --rescut 3.5 \
+    --batch_size 10000 \
+    --num_epochs 200 \
+    --learning_rate 3e-3 \
+    --weight_decay 1e-2 \
+    --gradient_norm 1.0 \
+    --dropout 0.1 \
+    --mixed_precision
