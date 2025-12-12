cff0ba27076953e7d4bdeec5d2873a5865aaadb4
FridrichMethod <zhaoyangli@stanford.edu>
2025-12-12 10:42:22 -0800

    Refactor training script and introduce new training module
    - Updated `train.sh` to call the new training module `mpnn.train` and adjusted training parameters, increasing the batch size from 10,000 to 2,000 and the number of epochs from 4 to 200 for improved model training.
    - Introduced a new `train.py` file containing the training logic, including data loading, model initialization, and training loop, enhancing code organization and maintainability.
    - Implemented various utility functions for loss calculation, model evaluation, and data handling within the new training module.

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@
 
 PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 
-python -m mpnn.training \
+python -m mpnn.train \
     --path_for_training_data "$PROJECT_ROOT_DIR"/datasets/pdb_2021aug02_sample/ \
     --output_dir "$PROJECT_ROOT_DIR"/checkpoints/train \
     --optimizer adamw \
@@ -15,8 +15,8 @@ python -m mpnn.training \
     --backbone_noise 0.2 \
     --rescut 3.5 \
     --num_workers 8 \
-    --batch_size 10000 \
-    --num_epochs 4 \
+    --batch_size 2000 \
+    --num_epochs 200 \
     --learning_rate 3e-3 \
     --weight_decay 1e-2 \
     --gradient_norm 1.0 \
